d7817e275f5f6a782f3ed8085c41115e34bf56dc
Yoga Pradika <yogapradikaa10@gmail.com>
2026-01-31 01:01:05 +0700

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Videographer", "Videoeditor", "Streaming Crew"],
+    strings: ["Videographer", "Videoeditor", "Content Creator", "Streaming Crew"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
